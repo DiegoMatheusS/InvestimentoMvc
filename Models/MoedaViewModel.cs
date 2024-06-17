3e4822098d6259a2d1a9a0d280e363efbcfdcb11
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2024-06-17 18:01:14 +0000

    adicionado model enuns

--- Models/MoedaViewModel.cs
+++ Models/MoedaViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Investimento.Models.Enuns;
 
 namespace InvestimentoMvc.Models
 {
@@ -11,11 +12,12 @@ namespace InvestimentoMvc.Models
         public string Name { get; set; } = string.Empty;
         public double Valor { get; set; }
         public double Quantidade { get; set; }
-        public DateTime DataCompra { get; set; }
-        public DateTime DataVenda { get; set; }
+        public DateTime Data { get; set; }
         public double Lucro { get; set; }
         public double Prejuizo { get; set; }
         public double Imposto { get; set; }
+        public int? UsuarioId { get; set; }
+	    public OperacaoEnum Operacao { get; set; }
 
     }
 }
