b329c0da28e542e8448fa620d81ae4cc14a7176e
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2024-06-25 19:46:56 -0300

    Update UsuariosController.cs

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -13,7 +13,7 @@ namespace InvestimentoMvc.Controllers
 {
     public class UsuariosController : Controller
     {
-        public string uriBase => "https://invesntimentomvc.azurewebsites.net";
+        public string uriBase => "https://investimentomvc.azurewebsites.net/";
 
         [HttpGet]
         public ActionResult Index()
